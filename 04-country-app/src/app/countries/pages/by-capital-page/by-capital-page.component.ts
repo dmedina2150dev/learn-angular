cfc7d381d16186b09e6774672231a7686f505fb7
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-09 21:03:02 -0400

    refactorizaciones y loading

--- 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -11,15 +11,18 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
     public countries: Country[] = [];
+    public isLoading: boolean = false;
 
     constructor( private countriesService: CountriesService ) { }
 
     searchByCapital( term: string ) {
+        this.isLoading = true;
         // console.log("Dese ByCapitalPage");
         // console.log({ term });
         this.countriesService.serachCapital( term )
             .subscribe( countries => {
                 this.countries = countries;
+                this.isLoading = false;
             });
     }
 }
