0e192e60bf5cb210d11389904ae13676edb122c1
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-09 21:02:40 -0400

    refactorizacion services

--- 04-country-app/src/app/countries/services/countries.service.ts
+++ 04-country-app/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +9,16 @@ export class CountriesService {
     private apiUrl: string = 'https://restcountries.com/v3.1';
 
     constructor( private _http: HttpClient ) { }
+
+    //* Refactorized
+    private getCountriesRequest( url: string ): Observable<Country[]> {
+        return this._http.get<Country[]>( url )
+            .pipe(
+                catchError( error => of([]) ),
+                delay(2000)
+            );
+    }
+
 
     searchCountryByAlphaCode( code: string ): Observable<Country | null> {
         return this._http.get<Country[]>(`${ this.apiUrl }/alpha/${ code }`)
@@ -19,23 +29,17 @@ export class CountriesService {
     }
 
     serachCapital( term: string ): Observable<Country[]> {
-        return this._http.get<Country[]>(`${ this.apiUrl }/capital/${ term }`)
-            .pipe(
-                catchError( error => of([]) )
-            );
+        const url = `${ this.apiUrl }/capital/${ term }`;
+        return this.getCountriesRequest( url );
     }
 
     searchCountry( term: string ): Observable<Country[]> {
-        return this._http.get<Country[]>(`${ this.apiUrl }/name/${ term }`)
-            .pipe(
-                catchError( error => of([]) )
-            );
+        const url = `${ this.apiUrl }/name/${ term }`;
+        return this.getCountriesRequest( url );
     }
 
     searchByRegion( region: string ): Observable<Country[]> {
-        return this._http.get<Country[]>(`${ this.apiUrl }/region/${ region }`)
-            .pipe(
-                catchError( error => of([]) )
-            );
+        const url = `${ this.apiUrl }/region/${ region }`;
+        return this.getCountriesRequest( url );
     }
 }
