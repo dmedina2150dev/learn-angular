6860d7510a6aba3ca23922146b006da7b9a502ff
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-16 13:07:54 -0400

    Pipe i18nSelect

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+    // i18n Selected
+    public name: string = 'Fernando';
+    public gender: 'male' | 'female' = 'male';
+    public invitationMap = {
+        male: 'invitarlo',
+        female: 'invitarla'
+    }
+
+    changeClient(): void {
+        this.name = "Dajan";
+        this.gender = "female";
+    }
 }
