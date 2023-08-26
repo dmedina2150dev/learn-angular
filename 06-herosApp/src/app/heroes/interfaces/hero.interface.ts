d13ba6aaf399104f24efbaafd76c74e69a52a201
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-25 20:49:48 -0400

    Agregamos las imagenes de los heroes

--- 06-herosApp/src/app/heroes/interfaces/hero.interface.ts
+++ 06-herosApp/src/app/heroes/interfaces/hero.interface.ts
@@ -5,8 +5,9 @@ export interface Hero {
     superhero:        string;
     publisher:        Publisher;
     alter_ego:        string;
-    first_appearance: string;
+    first_appearance:  string;
     characters:       string;
+    alt_img?:         string;
 }
 
 export enum Publisher {
