b8c2a50959c9ec661d198dbd6688bcdf48183c9d
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-26 17:16:12 -0400

    Pagina buscar un heroe

--- 06-herosApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-herosApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styles: [
-  ]
+    selector: 'app-search-page',
+    templateUrl: './search-page.component.html',
+    styles: [
+    ]
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnInit {
+
+    public searchInput = new FormControl('');
+    public heroes: Hero[] = [];
+    public selectedHero?: Hero;
+
+    constructor(
+        private heroesService: HeroesService
+    ) {}
+
+    ngOnInit(): void {
+
+    }
+
+    searchHero() {
+        const value: string = this.searchInput.value || '';
+
+        if ( value ) this.heroes = [];
+
+        // console.log({ value });
+
+        this.heroesService.getSuggestions(value)
+            .subscribe( heroes => this.heroes = heroes );
+
+    }
+
+    onSelectedOption( event: MatAutocompleteSelectedEvent ): void {
+        // console.log( event.option.value );
+
+        if( !event.option.value ) {
+            this.selectedHero = undefined;
+            return;
+        }
+
+        const hero: Hero = event.option.value;
+
+        this.searchInput.setValue( hero.superhero );
+        this.selectedHero = hero;
+
+        console.log( this.searchInput.value )
+    }
 
 }
