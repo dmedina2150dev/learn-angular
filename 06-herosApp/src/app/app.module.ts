29497e9ac2d2297eda7a482adeb9b9e555a8485c
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-25 20:14:32 -0400

    Cargamos nuestros heroes desde un servicio

--- 06-herosApp/src/app/app.module.ts
+++ 06-herosApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        HttpClientModule,
 
         AppRoutingModule,
         SharedModule,
