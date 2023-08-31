72f4e5069e1e68c2e90d2bf736d781150ccf9494
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-30 20:03:39 -0400

    Creamos dos formas de crear formularios reactivos

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+    templateUrl: './basic-page.component.html',
+    styles: [
+    ]
 })
 export class BasicPageComponent {
 
+    // * Una forma de crear formularios reactivos
+    public myForm: FormGroup = new FormGroup({
+        // Todo: El primer argumento es el valor
+        // Todo: El segundo argumento validaciones sincronas
+        // Todo: El tercero argumento validaciones asincronas
+        name: new FormControl('', [], []),
+        price: new FormControl(0, [], []),
+        inStorage: new FormControl(0, [], []),
+    });
+
+    // * Otra forma de crear formularios reactivos
+    // public myForm2: FormGroup = this._fb.group({
+    //     // Todo: El primer argumento es el valor
+    //     // Todo: El segundo argumento validaciones sincronas
+    //     // Todo: El tercero argumento validaciones asincronas
+    //     name: ['', [], []],
+    //     price: [0, [], []],
+    //     inStorage: [0, [], []],
+    // });
+
+    constructor(
+        private _fb: FormBuilder,
+    ){}
+
+    onSave(): void {
+        console.log(this.myForm.value);
+    }
 }
