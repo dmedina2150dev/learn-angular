2e9bd60a709eaab6e4536fdf89828278c0f53233
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-30 21:18:47 -0400

    Creamos formulario dinamico y mostramos los valores

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+    templateUrl: './dynamic-page.component.html',
+    styles: [
+    ]
 })
 export class DynamicPageComponent {
 
+    // * La otra forma de hacer el form array
+    // public myForm2: FormGroup = new FormGroup({
+    //     favoritesGames: new FormArray([]),
+    // })
+
+    public myForm: FormGroup = this._fb.group({
+        name: ['', [ Validators.required, Validators.minLength(3)]],
+        favoriteGames: this._fb.array([
+            ['Metal Gear', Validators.required ],
+            ['Death Stranding', Validators.required ],
+        ]),
+
+    });
+
+    constructor( private _fb: FormBuilder ) {
+
+    }
+
+    get favoriteGamesControls() {
+        return this.myForm.get('favoriteGames') as FormArray;
+    }
+
+    onSubmit(): void {
+
+        if ( this.myForm.invalid ) {
+            this.myForm.markAllAsTouched();
+            return;
+        }
+
+        console.log(this.myForm.value);
+        this.myForm.reset();
+    }
 }
