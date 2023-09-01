fc42c0dc46c8f22899fe1f10deb9bcf7a06b38f6
Dajan Medina <dmedina2150dev@gmail.com>
2023-08-31 21:14:12 -0400

    Creamos la implementacion de form en register

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './register-page.component.html',
-  styles: [
-  ]
+    templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
+
+    public myForm: FormGroup = this._fb.group({
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required]],
+        username: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        password2: ['', [Validators.required]],
+    });
+
+    constructor(private _fb: FormBuilder) { }
+
+    isValidField(field: string): boolean | null {
+        // return this.myForm.controls[ field ].errors
+        //     && this.myForm.controls[ field  ].touched;
+        // TODO: Obtener de un servicio
+        return true;
+    }
+
+    onSubmit(): void {
+
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched();
+            return;
+        }
+
+        console.log(this.myForm.value);
+    }
 
 }
